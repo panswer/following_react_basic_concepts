5794002a9ce9b8bc1b1f59619a90ba1446653e59
Ricardo Mejias <panswer.rjmm@gmail.com>
2021-02-15 16:15:20 -0400

    update: render an element

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-function formatName(user={
-  firstName:'',
-  lastName:''
-}) {
-  return user.firstName+' '+user.lastName;
-}
+const element=<h1>Hello, world</h1>;
 
-const user={
-  firstName:'Harper',
-  lastName:'Perez'
-};
-
-ReactDOM.render(
-  <h1>Hello, {formatName(user)}</h1>,
-  document.getElementById('root')
-);
+ReactDOM.render(element,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
