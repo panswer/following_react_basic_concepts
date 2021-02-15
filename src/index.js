e959c66a00a1bf2d45ad5e116dc7145146cadc59
Ricardo Mejias <panswer.rjmm@gmail.com>
2021-02-15 16:19:34 -0400

    finish: Renderizando elementos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-const element=<h1>Hello, world</h1>;
+function tick() {
+  const element=(
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It's {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
 
-ReactDOM.render(element,document.getElementById('root'));
+  ReactDOM.render(element,document.getElementById('root'));
+}
+
+setInterval(tick,1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
