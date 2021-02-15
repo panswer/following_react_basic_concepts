566323d1525e7f9b16b24a8ff18881a6ec38c1b6
Ricardo Mejias <panswer.rjmm@gmail.com>
2021-02-15 16:01:41 -0400

    rendering a variable

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const name='Josh Perez';
+const element = <h1>Hello, {name}</h1>;
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
 
