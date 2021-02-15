cd96ebbe614c6eb2a2386974d1ee7e2e25b0e980
Ricardo Mejias <panswer.rjmm@gmail.com>
2021-02-15 16:11:31 -0400

    finish: presentando JSX

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-const name='Josh Perez';
-const element = <h1>Hello, {name}</h1>;
+function formatName(user={
+  firstName:'',
+  lastName:''
+}) {
+  return user.firstName+' '+user.lastName;
+}
+
+const user={
+  firstName:'Harper',
+  lastName:'Perez'
+};
 
 ReactDOM.render(
-  element,
+  <h1>Hello, {formatName(user)}</h1>,
   document.getElementById('root')
 );
 
