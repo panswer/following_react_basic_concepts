52ba60687ab4f92e7f993b53422fe8ecf6f5ac39
Ricardo Mejias <panswer.rjmm@gmail.com>
2021-02-15 21:34:20 -0400

    escribiendo functiones de conversion

--- src/components/class/Calculator.jsx
+++ src/components/class/Calculator.jsx
@@ -3,6 +3,14 @@ import { Component } from "react";
 // import BoilingVerdict from "../function/BoilingVerdict";
 import TemperatureInput from "./TemperatureInput";
 
+function toCelsius(fahrenheit) {
+    return (fahrenheit -32) * 5 /9;
+}
+
+function toFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
 class Calculator extends Component{
     render(){
         return (
