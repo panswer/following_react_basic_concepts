90847ec5ab03587c699631a5fb64675f41344b2e
Ricardo Mejias <panswer.rjmm@gmail.com>
2021-02-15 21:59:31 -0400

    levantando el estado

--- src/components/class/TemperatureInput.jsx
+++ src/components/class/TemperatureInput.jsx
@@ -9,21 +9,23 @@ class TemperatureInput extends Component{
     constructor(props){
         super(props);
 
-        this.state={
-            temperature:''
-        };
+        // this.state={
+        //     temperature:''
+        // };
 
         this.handleChange=this.handleChange.bind(this);
     }
 
     handleChange(e){
-        this.setState({
-            temperature:e.target.value
-        });
+        // this.setState({
+        //     temperature:e.target.value
+        // });
+        this.props.onTemperatureChange(e.target.value);
     }
 
     render(){
-        const temperature=this.state.temperature;
+        // const temperature=this.state.temperature;
+        const temperature=this.props.temperature;
         const scale=this.props.scale;
 
         return (
